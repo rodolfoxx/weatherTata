9c88be75a358125887ac2e13fb9dc151f3820e33
p5mur_z@hotmail.com <makita23>
2022-03-24 13:52:21 -0300

    se arreglo test

--- src/main/java/com/test/tataconsultancy/weather/controller/WeatherController.java
+++ src/main/java/com/test/tataconsultancy/weather/controller/WeatherController.java
@@ -3,8 +3,8 @@ package com.test.tataconsultancy.weather.controller;
 import com.test.tataconsultancy.weather.dto.WeatherRequest;
 import com.test.tataconsultancy.weather.dto.WeatherResponse;
 import com.test.tataconsultancy.weather.service.IWeatherService;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +17,7 @@ public class WeatherController {
         this.weatherService = weatherService;
     }
 
-    @RequestMapping("/weather")
+    @PostMapping("/weather")
     public WeatherResponse weather(@RequestBody WeatherRequest weather) {
         System.out.println("==== in weather ====");
          return weatherService.dataWeather(weather);
